87cb2f14f3b227fdc52068681c66652aabcfdb93
Tarun <tarun@belgiumwebnet.com>
2025-01-23 19:11:28 +0530

    Done Happy clinet component

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';  
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';  
+import App from './App.jsx';
 import './index.css';
 import './assets/css/style.css';
- 
+
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   );
 } else {
   console.error("Root element not found. Please check your HTML file.");
